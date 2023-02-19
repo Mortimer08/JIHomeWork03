b5eb286d55a8b481538dbeb8bf650ae1a35f3ca3
Ilya Makarov <ilyam2003@mail.ru>
2023-02-19 19:18:23 +0500

    Task01 merging -> method Comments added

--- jihomework03/src/main/java/com/example/task01.java
+++ jihomework03/src/main/java/com/example/task01.java
@@ -28,45 +28,55 @@ public class task01 {
         arrSubLeft = mergeSorting(arrSubLeft);
 
         arrSubRight = mergeSorting(arrSubRight);
-
-        int leftArrIndex = 0;
-        int rightArrIndex = 0;
-
-        while (leftArrIndex < arrSubLeft.size() && rightArrIndex < arrSubRight.size()) {
-
-            if (arrSubLeft.get(leftArrIndex) < arrSubRight.get(rightArrIndex)) {
-                arr.set(leftArrIndex + rightArrIndex, arrSubLeft.get(leftArrIndex));
-
-                leftArrIndex++;
-            } else {
-                arr.set(leftArrIndex + rightArrIndex, arrSubRight.get(rightArrIndex));
-
-                rightArrIndex++;
-            }
-
-        }
-        if (leftArrIndex < arrSubLeft.size()) {
-
-            for (int i = leftArrIndex; i < arrSubLeft.size(); i++) {
-                arr.set(leftArrIndex + rightArrIndex, arrSubLeft.get(i));
-                leftArrIndex++;
-            }
-        } else {
-
-            for (int i = rightArrIndex; i < arrSubRight.size(); i++) {
-                arr.set(leftArrIndex + rightArrIndex, arrSubRight.get(i));
-                leftArrIndex++;
-            }
-        }
-
+        arrMerge(arr, arrSubLeft, arrSubRight);
         return arr;
     }
 
-    public static void arrMerge(List<Integer> arrLeft, List<Integer> arrRight) {
+    public static void arrMerge(List<Integer> arrMain, List<Integer> arrLeft, List<Integer> arrRight) {
+        /*
+         * Merging two subarrays
+         */
+        int leftArrIndex = 0;
+        int rightArrIndex = 0;
 
+        while (leftArrIndex < arrLeft.size() && rightArrIndex < arrRight.size()) {
+            /*
+             * Проходим по подмассивам до тех пор, пока элементы из один из них не будут полностью
+             * перенесены в основной массив 
+             */
+            if (arrLeft.get(leftArrIndex) < arrRight.get(rightArrIndex)) {
+                /*
+                 * В основной массив переносится меньший из двух рассматриваемых
+                 * (один - из левого и один - из правого подмассивов)
+                 */
+                arrMain.set(leftArrIndex + rightArrIndex, arrLeft.get(leftArrIndex));
+                leftArrIndex++;
+            } else {
+                arrMain.set(leftArrIndex + rightArrIndex, arrRight.get(rightArrIndex));
+                rightArrIndex++;
+            }
+        }
+        if (leftArrIndex < arrLeft.size()) {
+            /*
+             * Когда один из подмассивов закончился, элементы, которые остались не перенесёнными
+             * (из второго подмассива) полностью переносятся в конец основного массива 
+             */
+            for (int i = leftArrIndex; i < arrLeft.size(); i++) {
+                arrMain.set(leftArrIndex + rightArrIndex, arrLeft.get(i));
+                leftArrIndex++;
+            }
+        } else {
+            for (int i = rightArrIndex; i < arrRight.size(); i++) {
+                arrMain.set(leftArrIndex + rightArrIndex, arrRight.get(i));
+                leftArrIndex++;
+            }
+        }
     }
 
     public static void intListRandomFilling(List<Integer> arr, int size) {
+        /*
+         * Заполнение массива случайными целыми
+         */
         Random rnd = new Random();
         for (int i = 0; i < size; i++) {
             arr.add(rnd.nextInt(100));
